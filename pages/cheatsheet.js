ccbd772c5f1984cd19b454e78696fdf060de2f9c
Greg Millam <captdeaf@gmail.com>
2024-06-26 20:01:53 -0700

    Initial pages commit to get started w/ github

---
+++ pages/cheatsheet.js
@@ -0,0 +1,9 @@
+// cheatsheet.js
+//
+// UI management for plcheat, a programming language cheat sheet website.
+//
+// Using highlight.js for ... pretty much everything.
+//
+// Programming (default): C, Bash, python, rust, etc.
+//
+// Database: pgsql, sql, etc.
